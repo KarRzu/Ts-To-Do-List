5c1a3f606d575ad1ecab6cfd2d4394454343adb0
Karolina <karola.rzucidlo14@gmail.com>
2024-04-26 18:41:36 +0200

    feat: add task form

---
+++ src/features/task/task-form/TaskForm.ts
@@ -0,0 +1,41 @@
+import { TaskState, Task, TaskPriority, StoryPoints } from "../types";
+
+export class TaskForm{
+    
+    /*title: HTMLInputElement;
+    description :HTMLTextAreaElement;
+    taskPriority:HTMLSelectElement;
+    taskStoryPoints:HTMLSelectElement;
+    btnCreate: HTMLButtonElement;
+
+    constructor(){
+        this.initialize();
+    
+    }
+
+    initialize(){
+        this.title = document.getElementById("title") as HTMLInputElement;
+        this.description = document.getElementById("description") as HTMLTextAreaElement;
+        this.taskPriority = document.getElementById("task-priority") as HTMLSelectElement;
+        this.taskStoryPoints = document.getElementById("task-story-points") as HTMLSelectElement;
+        this.btnCreate = document.getElementById("btn-create") as HTMLButtonElement;
+    }
+
+    createTask(){
+    const titleValue = this.title.value;
+    const descriptionValue = this.description.value;
+    const taskPriorityValue: TaskPriority = this.taskPriority.value as TaskPriority;
+    const taskStoryPoints: StoryPoints = this.taskStoryPoints.value as StoryPoints;
+    const timestamp = new Date().toISOString();
+    const taskState: TaskState = 'CREATED';
+    const newTask: Task = {
+        title: titleValue,
+        description: descriptionValue,
+        priority: taskPriorityValue ,
+        state: taskState,
+        storyPoints: taskStoryPoints,
+        timestamp: timestamp,
+        id: Math.floor(Math.random() * 1000)
+    }
+    }*/
+}
