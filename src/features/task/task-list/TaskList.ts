91fa9930c36633a8669bda6973a32984dd1d4fdb
Karolina <karola.rzucidlo14@gmail.com>
2024-05-18 23:24:06 +0200

    feat: add edit-task

--- src/features/task/task-list/TaskList.ts
+++ src/features/task/task-list/TaskList.ts
@@ -3,7 +3,8 @@ import { createListItem } from "./TaskItem";
 
 export class TaskList {
     taskListElement: HTMLDivElement;
-
+    deleteButtons: HTMLButtonElement[];
+    editButtons: HTMLButtonElement[];
     tasks: Task[] = [];
 
 
@@ -11,11 +12,49 @@ export class TaskList {
     this.initialiazeTaskList();
     }
 
-   async getTasks(){
+    initialiazeTaskList(){
+        this.taskListElement = document.querySelector(".task-list") as HTMLDivElement; 
+        this.getTasks();
+    }
+
+    
+    render(){
+
+        this.tasks.forEach((element) => {
+            const taskItem = createListItem(element);
+            this.taskListElement.appendChild(taskItem);
+        })
+
+
+        const deleteButtons = document.querySelectorAll(".btn-delete");
+        
+        console.log(deleteButtons);
+
+        deleteButtons.forEach((element) => {
+            element.addEventListener("click" , (event) => {
+                console.log("klik");
+            this.deleteTask(event)
+            console.log(this.deleteTask)
+            })
+        })
+
+
+        const editButtons = document.querySelectorAll(".btn-edit");
+    
+    
+        editButtons.forEach((element) => {
+            element.addEventListener("click", (event) => {
+            this.editTask(event);
+            });
+        });
+    } 
+
+async getTasks(){
 
     try{
         const response = await fetch("http://localhost:3000/tasks", {
             method: "GET",
+            headers: {'Content-type': 'application/json'}
         })
         .then((response) =>{
         return response.json() as Promise<Task[]>;
@@ -30,15 +69,69 @@ export class TaskList {
 
     }
 
-    initialiazeTaskList(){
-        this.taskListElement = document.querySelector(".task-list") as HTMLDivElement; 
-        this.getTasks();
+    async deleteTask(event: Event){
+        console.log("hello")
+
+        const target = event.target as HTMLElement
+        console.log(target)
+        const id: number = Number(target.getAttribute('data-id'))
+        console.log(id)
+
+        const URL: string = "http://localhost:3000/tasks"
+
+        try{
+            const response = await fetch(`${URL}/${id}`, {
+            method: "DELETE",
+            headers: {'Content-type': 'application/json'}
+            })
+
+            if(response.ok){ 
+                //this.render();
+                this.getTasks();
+            }
+            
+        }
+        catch(error){
+          console.log(error)
+        }
+
     }
 
-    render(){
-        this.tasks.forEach((element) => {
-            const taskItem = createListItem(element);
-            this.taskListElement.appendChild(taskItem);
-        })
+
+    async editTask(event: Event) {
+
+        const target = event.target as HTMLElement
+        console.log(target)
+        const id: number = Number(target.getAttribute('data-id'))
+        console.log(id)
+       
+
+         const input = document.querySelectorAll('.form-control.task-list__input');
+         const textarea = document.querySelectorAll('.form-control.task-list__textarea');
+
+    input.forEach((element: HTMLInputElement) => {
+        element.disabled = false;
+    });
+    
+    textarea.forEach((element: HTMLTextAreaElement) => {
+        element.readOnly = false;
+    });
+
+    const btnEdit = document.querySelector('.btn-edit') as HTMLButtonElement;
+
+    if (btnEdit) {
+        btnEdit.disabled = true;
     }
+
 }
+
+  enterTask(){
+    const enterBtn = document.querySelector("btn-enter");
+
+    enterBtn?.addEventListener("click", () => {
+        
+    })
+  }
+
+    }
+
